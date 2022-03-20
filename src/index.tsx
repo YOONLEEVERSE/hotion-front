edaed19d834c681d859a9213f31fc21bef8ad3f1
bebusl <bebus1998@naver.com>
2022-03-20 21:18:01 +0900

    Feat: Apollo client 세팅 및 글로벌 스타일 적용

--- src/index.tsx
+++ src/index.tsx
@@ -12,18 +12,83 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import store from "./redux/store";
+import { createUploadLink } from "apollo-upload-client";
 
 //global style
 const GlobalStyle = createGlobalStyle`
-  body{
-    background-color: black;
-    color:white;
-  }
+html, body, div, span, applet, object, iframe,
+ p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+body{
+  margin:0;
+}
+/* input 기본 스타일 초기화 */
+input {
+    -webkit-appearance: none;
+       -moz-appearance: none;
+            appearance: none;
+      border:1px solid #ededed;
+}
+
+input:-internal-autofill-selected {
+  background-color:#ededed!important;
+}
+/* IE10 이상에서 input box 에 추가된 지우기 버튼 제거 */
+input::-ms-clear { display: none; }
+
+/* input type number 에서 화살표 제거 */
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+       -moz-appearance: none;
+            appearance: none;
+}
+
 `;
 
 //apollo-client config
-const httpLink = createHttpLink({
-  uri: "https://www.temp.com/graphql",
+const httpLink = createUploadLink({
+  uri: "https://ordinary-panda-67.loca.lt/graphql",
   credentials: "same-origin",
 }); //파일 업로드일 경우 createHttpLink => apollo-upload-client의 createUploadLink로 바꿀 것.
 
