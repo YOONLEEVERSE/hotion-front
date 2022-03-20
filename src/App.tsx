475028ee49a0b9a1203b625bc3204b95caee918c
bebusl <bebus1998@naver.com>
2022-03-20 21:17:27 +0900

    Feat: 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import Welcome from "./pages/welcome";
+import { useSelector, useDispatch } from "react-redux";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Welcome</header>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/main/:username" element={<Main />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
     </div>
   );
 }
