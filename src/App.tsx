ea04a6471315da8971270718805e4adb005dbd4a
bebusl <bebus1998@naver.com>
2022-03-29 00:25:42 +0900

    Chore: 노트 라우팅 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Note from "./pages/Note";
-import Welcome from "./pages/welcome";
-import { useSelector, useDispatch } from "react-redux";
-import Signup from "./pages/signup";
-import Login from "./pages/login";
+import Note from "./routes/Note";
+import Welcome from "./routes/welcome";
+import Signup from "./routes/signup";
+import Login from "./routes/login";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Welcome />}></Route>
-        <Route path="/note/:username" element={<Note />}></Route>
+        <Route path="/note/:username/:currentPage" element={<Note />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
       </Routes>
